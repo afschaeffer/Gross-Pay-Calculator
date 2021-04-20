4f10cf90193d81ec9d7f87bc0ffcc6dc9ab7b012
Amy F Schaeffer <65150396+afschaeffer@users.noreply.github.com>
2021-04-20 12:15:09 -0700

    Add files via upload

---
+++ Assignment_2_Schaeffer.cs
@@ -0,0 +1,40 @@
+﻿// Amy Schaeffer
+// Program that calculates and displays gross pay based on user inputs.
+using System;
+
+namespace Schaeffer_Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int hours;
+            int rate;
+            double gross_pay;
+
+
+            while (++count <= 3)
+            {
+                Console.Write("Enter Hours Worked: ");
+                hours = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter Hourly Rate: ");
+                rate = Convert.ToInt32(Console.ReadLine());
+
+                if (hours <= 40)
+                {
+                    gross_pay = hours * rate;
+                }
+                else
+                {
+                    gross_pay = (rate * 40) + ((hours - 40) * (rate * 1.5));
+                }
+
+                Console.WriteLine($"Your Gross Pay: ${gross_pay}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
